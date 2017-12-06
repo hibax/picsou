7056233f89c7fff961b634a4507f71dde716b689
hibax <julienflb.iie@gmail.com>
2017-12-07 00:34:41 +0100

    example using btfxwss

--- feedhandler/src/main/main.py
+++ feedhandler/src/main/main.py
@@ -1,5 +1,9 @@
 import json
 
+import logging
+import time
+import sys
+
 from websocket import create_connection
 
 from model.src.main.book.book import Book
@@ -7,8 +11,14 @@ from model.src.main.strategy.mbl import MBL
 from model.src.main.strategy.mbl_snapshot import MBLSnapshot
 from model.src.main.strategy.mbl_update import MBLUpdate
 
+from btfxwss import BtfxWss
+
 
 def main():
+    btfxwss()
+
+
+def home_made_websocket():
     ws = create_connection("wss://api.bitfinex.com/ws/2")
 
     # Waiting connection
@@ -44,6 +54,7 @@ def main():
     mbl = MBL()
     book = Book()
 
+
     while True:
         result = ws.recv()
         result = json.loads(result)
@@ -61,7 +72,62 @@ def main():
             book = mbl.update(book, update)
             print(book)
 
-    ws.close()
+
+def btfxwss():
+
+    log = logging.getLogger(__name__)
+
+    fh = logging.FileHandler('test.log')
+    fh.setLevel(logging.DEBUG)
+    sh = logging.StreamHandler(sys.stdout)
+    sh.setLevel(logging.DEBUG)
+
+    log.addHandler(sh)
+    log.addHandler(fh)
+    logging.basicConfig(level=logging.DEBUG, handlers=[fh, sh])
+
+    wss = BtfxWss()
+    wss.start()
+
+    while not wss.conn.connected.is_set():
+        time.sleep(1)
+
+    # Subscribe to some channels
+    wss.subscribe_to_ticker('BTCUSD')
+    wss.subscribe_to_order_book('BTCUSD')
+    wss.subscribe_to_order_book('ETHUSD')
+
+    # Wait for subscription...
+    time.sleep(2)
+
+    mbl = MBL()
+    book = Book()
+
+    # Accessing data stored in BtfxWss:
+    books_queue = wss.books('ETHUSD')  # returns a Queue object for the pair.
+
+    while True:
+
+        book_update = books_queue.get()[0][0]
+
+        if len(book_update) > 3:
+            print("snapshot")
+            mbl_snapshot = MBLSnapshot(book_update)
+            book = mbl.from_snapshot(mbl_snapshot)
+            print(book)
+
+        elif len(book_update) == 3:
+            print("update: " + str(book_update))
+            update = MBLUpdate(book_update[0], book_update[1], book_update[2])
+            book = mbl.update(book, update)
+            print(book)
+
+    # Unsubscribing from channels:
+    wss.unsubscribe_from_ticker('BTCUSD')
+    wss.unsubscribe_from_order_book('BTCUSD')
+
+    # Shutting down the client:
+    wss.stop()
 
 
 if __name__ == '__main__':
